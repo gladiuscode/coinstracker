f38404a0dfb1f3631e0a3505c3ffb3c8e6ba6de5
gladius <gladiusservices@protonmail.com>
2022-04-05 21:49:37 +0200

    FEATURE HOME: Add favourite icons and update assets dir structure and usages

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,3 +1,4 @@
 export enum Images {
-  ic_settings = require('./images/settings.png'),
+  ic_settings = require('./images/settings/settings.png'),
+  ic_favourite = require('./images/favourite/favourite.png'),
 }
