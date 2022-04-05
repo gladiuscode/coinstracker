2f532e906d8b9697185be19f337d66ce5dfafb7e
gladius <gladiusservices@protonmail.com>
2022-04-05 19:38:51 +0200

    FEATURE HOME: Wrap App in RecoilRoot

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import MainStack from './navigation/stacks/main.stack';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import StylesProvider from './styles/provider/styles.provider';
 import LocalizationProvider from './localization/provider/localization.provider';
+import {RecoilRoot} from 'recoil';
 
 const App: React.FC = () => {
   return (
     <SafeAreaProvider>
       <StylesProvider>
         <LocalizationProvider>
-          <NavigationContainer>
-            <MainStack />
-          </NavigationContainer>
+          <RecoilRoot>
+            <NavigationContainer>
+              <MainStack />
+            </NavigationContainer>
+          </RecoilRoot>
         </LocalizationProvider>
       </StylesProvider>
     </SafeAreaProvider>
