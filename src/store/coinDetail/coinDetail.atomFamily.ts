4544a89abf8b62f215d1c2ccb5677dc788b868e8
gladius <gladiusservices@protonmail.com>
2022-04-06 18:46:37 +0200

    FEATURE HOME: Improve coinDetail atomFamily code quality

--- src/store/coinDetail/coinDetail.atomFamily.ts
+++ src/store/coinDetail/coinDetail.atomFamily.ts
@@ -7,16 +7,20 @@ import {ReadOnlySelectorFamilyOptions, selectorFamily} from 'recoil';
 import ApiLayer from '../../api/apiLayer.api';
 import {ApiPaths} from '../../api/paths.api';
 
-const get: ReadOnlySelectorFamilyOptions<CoinDetail, any>['get'] =
-  (id: string) => async () => {
+const get: ReadOnlySelectorFamilyOptions<CoinDetail, string>['get'] =
+  id => async () => {
     const apiLayer = new ApiLayer<GetCoinDetailRequestData>({
       url: ApiPaths.getCoinDetail,
       method: 'get',
-    }).setData({
+    });
+
+    const requestData: GetCoinDetailRequestData = {
       ids: id,
       vs_currencies: 'usd',
       include_last_updated_at: true,
-    });
+    };
+    apiLayer.setData(requestData);
+
     const response = await apiLayer.run<GetCoinDetailResponseData<typeof id>>();
     return response[id];
   };
