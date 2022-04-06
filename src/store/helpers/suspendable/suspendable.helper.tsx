01274a8ca44da621d30bf4fa062d4a4023d4090f
gladius <gladiusservices@protonmail.com>
2022-04-06 19:25:58 +0200

    FEATURE HOME: Update suspendable with fallback parameter to customize fallback component

--- src/store/helpers/suspendable/suspendable.helper.tsx
+++ src/store/helpers/suspendable/suspendable.helper.tsx
@@ -1,10 +1,10 @@
 import React, {createElement, Suspense} from 'react';
 import Fallback from './fallback/fallback.suspendable';
 
-const suspendable = <T,>(component: React.FC<T>) => {
+const suspendable = <T,>(component: React.FC<T>, fallback = <Fallback />) => {
   return (props: T) => {
     const componentInit = createElement(component, props);
-    return <Suspense fallback={<Fallback />}>{componentInit}</Suspense>;
+    return <Suspense fallback={fallback}>{componentInit}</Suspense>;
   };
 };
 
